23db7c6614154f79fab2ac1bd65c0ae088307420
Samatt1991 <samat116rust@yandex.ru>
2019-09-25 17:24:36 +0300

    tz-1 done!

--- tz-1.js
+++ tz-1.js
@@ -5,9 +5,15 @@ const base = '../../../../MY_IMAGES';
 
 const readDir = (base, level) => {
   const files = fs.readdirSync(base);
-
+  fs.access('./myDir', err =>{
+    if (err) {
+      fs.mkdir('./myDir', () => {
+        console.log('myDir created!');
+      });
+    }
+  });
+  
   files.forEach(item => {
-    
     let localBase = path.join(base, item);
     
     let state = fs.statSync(localBase);
@@ -15,12 +21,11 @@ const readDir = (base, level) => {
     if (state.isDirectory()) {
       readDir(localBase, level + 1);
     } else {
-      fs.access(`./${dirName}`, (err) => {
+      fs.access(`./myDir/${dirName}`, (err) => {
         if (err) {
-          fs.mkdir(`./${dirName}`, () => {
+          fs.mkdir(`./myDir/${dirName}`, () => {
             
-            fs.link(localBase, `./${dirName}/${item}`, err => {
-              console.log(dirName);
+            fs.link(localBase, `./myDir/${dirName}/${item}`, err => {
               if (err) {
                 console.error(24,err.message);
               }
@@ -28,8 +33,7 @@ const readDir = (base, level) => {
           });
 
          } else {
-          fs.link(localBase, `./${dirName}/${item}`, err => {
-            console.log(dirName);
+          fs.link(localBase, `./myDir/${dirName}/${item}`, err => {
             if (err) {
               console.error(33,err.message);
             }
